521e5ce6218da6c428488ad7d6e1d46d21d8fba0
karonbland <karon_bland@gmx.com>
2025-03-03 13:40:52 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HelloWorld {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name = input.nextLine();
+    System.out.println("Hello, " + name + "!");
+  }
+}
